d47d25c81f863b908a3a037941b1ab9580273239
Langxxx <316230604@qq.com>
2019-03-21 19:22:00 +0800

    [WIP]Update OpenAPI

--- Sources/App/Setup/Constants.swift
+++ Sources/App/Setup/Constants.swift
@@ -8,9 +8,8 @@
 import Foundation
 
 struct Constants {
-    static let trelloDefaultURLString = "https://api.trello.com/1/boards/5a4f2698cc2294fce859dc65"
     static let errorID = "Trello"
 
-    static let bcCreateMessageURLString = "http://api.stage.bearychat.com/v1/message.create"
-    static let baseFormURLString = "http://chaojidiao.stage.bearychat.com:8866"
+    static let bcCreateMessageURLString = "https://api.bearychat.com/v1/message.create"
+    static let baseFormURLString = "http://trello.applet.bearychat.com"
 }
